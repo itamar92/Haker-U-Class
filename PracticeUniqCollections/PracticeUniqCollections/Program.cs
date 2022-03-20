9007d0a490d1dc08043f927292d3faee88ab2d5d
itamar92 <92876432+itamar92@users.noreply.github.com>
2022-03-20 20:30:18 +0200

    Add files via upload

---
+++ PracticeUniqCollections/PracticeUniqCollections/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Create a UniqueCollection class that implement ICollection interface
+ * ,Choose any collection class type to hold the data items, 
+ * allow to enter only unique items to the data collection */
+using PracticeUniqCollections;
+class Program
+{
+    static void Main()
+    {
+        UniqueCollection studentsList = new UniqueCollection();
+
+        Console.WriteLine("Adding students to list:  ");
+        studentsList.Add("Itamar");
+        studentsList.Add("Noa");
+        studentsList.Add("Ido");
+        studentsList.Add("Amir");
+
+        Print(studentsList);
+
+        // Testing same name
+        Console.WriteLine("Adding Ido again \n");
+        studentsList.Add("Ido");
+
+        Console.WriteLine("Removing Noa");
+        studentsList.Remove("Noa");
+
+        Print(studentsList);
+
+        Console.WriteLine("Removing Tomer");
+        studentsList.Remove("Tomer");
+
+    }
+
+
+    public static void Print(UniqueCollection list)
+    {
+        Console.WriteLine("\n Students List:");
+        foreach(string name in list)
+        {
+            Console.Write(name + ", ");
+        }
+        Console.WriteLine("\n");
+    }
+
+
+}
---
+++ PracticeUniqCollections/PracticeUniqCollections/UniqueCollection.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PracticeUniqCollections
+{
+    public class UniqueCollection : ICollection<string>
+    {
+        private List<string> innerCol;
+
+        public int Count { get { return innerCol.Count; } }
+
+        public UniqueCollection()
+        {
+            innerCol = new List<string>();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            IEnumerator enumerator = innerCol.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+        }
+
+        IEnumerator<string> IEnumerable<string>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+               
+        public bool Contains(string name)
+        {
+            bool found = false;
+            foreach (string str in innerCol)
+            {
+                if (str.Equals(name))
+                {
+                    found = true;
+                }
+            }
+            return found;
+        }
+
+        public void Add(string name)
+        {
+            if (!Contains(name))
+            {
+                innerCol.Add(name);
+            }
+            else
+            {
+                Console.WriteLine("A Student with that name is allready in the list");
+            }
+
+        }
+
+        public void Clear()
+        {
+            innerCol.Clear();
+        }
+
+        public bool Remove(string name)
+        {
+            bool result = false;
+
+           if(Contains(name))
+            {
+                innerCol.Remove(name);
+            }
+            else
+            {
+                Console.WriteLine("That name is not in the list");
+            }
+            return result;
+        }
+
+        public void CopyTo(string[] array, int arrayIndex)
+        {
+            array = innerCol.GetRange(arrayIndex, innerCol.Count).ToArray();
+        }
+
+       
+
+        public bool IsReadOnly { get { return false; } }
+
+    }
+}
+
