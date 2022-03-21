73ef347d5e70630586cec859563da49be8510893
itamar92 <92876432+itamar92@users.noreply.github.com>
2022-03-21 15:06:24 +0200

    Add files via upload

---
+++ SerializationPractice/SerializationPractice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using SerializationPractice;
+class Program
+{
+    public static string userFilePath = "students.json";
+
+    public static void Main()
+    {
+        SerializationToFile();
+
+        DeSerializationFromFile();
+
+    }
+
+    static void SerializationToFile()
+    {
+        Console.WriteLine("SerializeToFile \n");
+        string usersJsonString;
+        List<Users> userList = new List<Users>()
+        {
+            new Users() { Age = 45, Name = "Moshe"},
+            new Users() { Age = 34, Name = "David" },
+            new Users() { Age = 23, Name = "Raine" },
+            new Users() { Age = 41, Name = "Joe" },
+            new Users() { Age = 24, Name = "Dana" },
+            new Users() { Age = 35, Name = "Ramya" },
+        };
+
+        usersJsonString = JsonSerializer.Serialize(userList);
+        File.WriteAllText(userFilePath, usersJsonString);
+        Console.WriteLine(usersJsonString + "\n");
+
+    }
+
+    static void DeSerializationFromFile()
+    {
+        Console.WriteLine("DesrilalizeFromFile \n");
+        string userJasonString = File.ReadAllText(userFilePath);
+        List<Users> userList = JsonSerializer.Deserialize<List<Users>>(userJasonString);
+        Print(userList);
+
+        static void Print(List<Users> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item} {item.Payment()}");
+            }
+        }
+
+    }
+}
+
---
+++ SerializationPractice/SerializationPractice/Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace SerializationPractice
+{
+	public class Users
+	{
+        public string Name { get; set; }
+        public int  Age { get; set; }
+
+		public Users() { }
+
+        public Users(string name, int age)
+		{
+			Age = age;
+			Name = name;
+		}
+
+		public string Payment()
+        {
+			if (Age < 25)
+            {
+                return "need to pay 9,000 ";
+            }
+			else
+            {
+				return "need to pay 10,000 ";
+			}
+        }
+
+		public override string ToString()
+        {
+            return $"{Name}, {Age} ";
+        }
+
+		
+	}
+}
+
