9129124ef948ac42da7ca8f3c166eb903f596e8a
itamar92 <92876432+itamar92@users.noreply.github.com>
2022-03-21 14:23:11 +0200

    Add files via upload

---
+++ YieldPractice/YieldPractice/Agency.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace YieldPractice
+{
+    public class Agency 
+    {
+        public Car[] Cars { get; set; }
+
+        //public bool MoveNext()
+        //{
+        //    int position = 0;
+        //    position++;
+        //    return (position < Cars.Length);
+        //}
+
+
+        public IEnumerator GetEnumerator()
+        {
+            IEnumerator enumerator = Cars.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+        }
+
+        public Car[] GetCars(string maker)
+        {
+             return Cars.Where(c => c.Maker == maker).ToArray();
+
+        }
+
+        //public Car[] GetCars(int modelYear)
+        //{
+        //    return Cars.Where(c => c.ModelYear == modelYear).ToArray();
+        //}
+
+        public IEnumerable<Car> GetCars(int modelYear)
+        {
+            foreach(var car in Cars)
+            {
+                if (car.ModelYear == modelYear)
+                    yield return car;
+            }
+        }
+    }
+}
+
+
+
---
+++ YieldPractice/YieldPractice/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace YieldPractice
+{
+	public class Car 
+	{
+		public string Maker { get; set; }
+
+		public int ModelYear { get; set; }
+
+		public override string ToString()
+        {
+			return $"{ModelYear}, {Maker}";
+		}
+
+
+	}
+}
+
