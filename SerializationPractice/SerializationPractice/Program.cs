b7f427dd07a9243a1bf941d1ed19e21f17974ea9
itamar92 <92876432+itamar92@users.noreply.github.com>
2022-03-26 12:55:51 +0300

    Finished

--- SerializationPractice/SerializationPractice/Program.cs
+++ SerializationPractice/SerializationPractice/Program.cs
@@ -2,51 +2,163 @@
 using SerializationPractice;
 class Program
 {
-    public static string userFilePath = "students.json";
-
+    public const string FILEPATHE = "students.json";
+    public const int PAYMENT = 10000;
     public static void Main()
     {
-        SerializationToFile();
+        bool running = true;
+        string userInput;
+        string appMessage = @"
+            Choos an action to perform:
+            a - Add student
+            r - Remove student
+            p - Print student list
+            sp - Print student payment fee
+            q - Quit";
 
-        DeSerializationFromFile();
-
-    }
-
-    static void SerializationToFile()
-    {
-        Console.WriteLine("SerializeToFile \n");
-        string usersJsonString;
-        List<Users> userList = new List<Users>()
+        while (running)
         {
-            new Users() { Age = 45, Name = "Moshe"},
-            new Users() { Age = 34, Name = "David" },
-            new Users() { Age = 23, Name = "Raine" },
-            new Users() { Age = 41, Name = "Joe" },
-            new Users() { Age = 24, Name = "Dana" },
-            new Users() { Age = 35, Name = "Ramya" },
-        };
-
-        usersJsonString = JsonSerializer.Serialize(userList);
-        File.WriteAllText(userFilePath, usersJsonString);
-        Console.WriteLine(usersJsonString + "\n");
-
-    }
-
-    static void DeSerializationFromFile()
-    {
-        Console.WriteLine("DesrilalizeFromFile \n");
-        string userJasonString = File.ReadAllText(userFilePath);
-        List<Users> userList = JsonSerializer.Deserialize<List<Users>>(userJasonString);
-        Print(userList);
-
-        static void Print(List<Users> list)
-        {
-            foreach (var item in list)
+            Console.WriteLine(appMessage);
+            userInput = Console.ReadLine().ToLower();
+            switch (userInput)
             {
-                Console.WriteLine($"{item} {item.Payment()}");
+                case "a":
+                    AddUser();
+                    break;
+                case "r":
+                    Console.WriteLine("Enter user name to remove: ");
+                    RemoveUser(Console.ReadLine());
+                    break;
+                case "p":
+                    PrintUsers();
+                    break;
+                case "sp":
+                    PrintUsersPayement();
+                    break;
+                case "q":
+                    running = false;
+                    Console.WriteLine("closing  app, Good Bye! ");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
             }
         }
 
     }
+
+
+    public static void AddUser()
+    {
+        string name = "";
+        int age = 0;
+        while (name == "")
+        {
+            Console.WriteLine("Enter user name: ");
+            name = Console.ReadLine();
+            if (name == "")
+            {
+                Console.WriteLine("Name field have to include at leat 1 char, Please try again. ");
+            }
+        }
+        while (age == 0)
+        {
+            try
+            {
+                Console.WriteLine("Enter User Age:");
+                age = int.Parse(Console.ReadLine());
+                if (age <= 0)
+                {
+                    Console.WriteLine("Invalid age ");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Only digit are accepted, please try again", e.Message);
+            }
+        }
+        Users addUser = new Users(name, age);
+        List<Users> userList = DeSerializationFromFile();
+        foreach (Users user in userList)
+        {
+            if (user.Name.ToLower() == addUser.Name.ToLower())
+            {
+                Console.WriteLine($"Cannot add {addUser}. user name already exist");
+                return;
+            }
+        }
+        userList.Add(addUser);
+        Console.WriteLine($"{addUser} added successfully");
+        SerializationToFile(userList);
+        Console.Clear();
+    }
+    public static void RemoveUser(string userName)
+    {
+        Console.Clear();
+        bool removed = false;
+        List<Users> userList = DeSerializationFromFile();
+        foreach (Users user in userList)
+        {
+            if (user.Name.ToLower() == userName.ToLower())
+            {
+                removed = userList.Remove(user);
+                SerializationToFile(userList);
+                break;
+            }
+        }
+        if (removed)
+        { Console.WriteLine($"{userName} removed succesfully "); }
+        else
+        { Console.WriteLine($"Cannot remove {userName}, user not in list"); }
+
+    }
+
+    public static int Payment(int age)
+    {
+        if (age > 25)
+        {
+            return PAYMENT;
+        }
+        else
+            return PAYMENT / 10 * 9;
+    }
+
+    public static void PrintUsers()
+    {
+        List<Users> userList = DeSerializationFromFile();
+        foreach (var user in userList)
+        {
+            Console.WriteLine(user);
+        }
+    }
+
+    public static void PrintUsersPayement()
+    {
+        List<Users> userList = DeSerializationFromFile();
+        foreach (var item in userList)
+        {
+            Console.WriteLine($"{item} need to pay {Payment(item.Age)}");
+        }
+    }
+    static void SerializationToFile(List<Users> list)
+    {
+        Console.WriteLine("SerializeToFile \n");
+        string usersJsonString;
+
+        usersJsonString = JsonSerializer.Serialize(list);
+        File.WriteAllText(FILEPATHE, usersJsonString);
+        Console.WriteLine(usersJsonString + "\n");
+    }
+
+    public static List<Users> DeSerializationFromFile()
+    {
+        string userJasonString = File.ReadAllText(FILEPATHE);
+        List<Users> userList = JsonSerializer.Deserialize<List<Users>>(userJasonString);
+        return userList;
+    }
+
+
+
+
 }
 
--- SerializationPractice/SerializationPractice/Users.cs
+++ SerializationPractice/SerializationPractice/Users.cs
@@ -4,37 +4,25 @@ using System.Collections.Generic;
 
 namespace SerializationPractice
 {
-	public class Users
-	{
+    public class Users
+    {
         public string Name { get; set; }
-        public int  Age { get; set; }
+        public int Age { get; set; }
 
-		public Users() { }
+        public Users() { }
 
         public Users(string name, int age)
-		{
-			Age = age;
-			Name = name;
-		}
-
-		public string Payment()
         {
-			if (Age < 25)
-            {
-                return "need to pay 9,000 ";
-            }
-			else
-            {
-				return "need to pay 10,000 ";
-			}
+            Age = age;
+            Name = name;
         }
 
-		public override string ToString()
+        public override string ToString()
         {
             return $"{Name}, {Age} ";
         }
 
-		
-	}
+
+    }
 }
 
