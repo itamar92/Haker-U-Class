900369aca95ffe9b44c841c02830c367be2b6fdf
itamar92 <92876432+itamar92@users.noreply.github.com>
2022-04-20 19:45:53 +0300

    Add files via upload

---
+++ ChinOfResponsibilityPractice/ChinOfResponsibilityPractice/ChinOfResponsibility.cs
@@ -0,0 +1,121 @@
+﻿using System;
+namespace ChinOfResponsibilityPractice
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public bool HasProblam { get; set; }
+
+        public string Error { get; set; }
+
+    }
+
+    public abstract class Check
+    {
+        protected Check _Next;
+
+        public void SetNext(Check next)
+        {
+            _Next = next;
+        }
+
+        public abstract void HandleRequest(Car car);
+    }
+
+    public class BasicCarHandler :Check
+    {
+        public override void HandleRequest(Car car)
+        {
+            int testResult = new Random().Next(1, 10);
+            if (testResult <= 5)
+            {
+                car.HasProblam = true;
+                car.Error = "Problam found in Basic Check ";
+            }
+            else
+            {
+                car.HasProblam = false;
+                if(_Next !=null)
+                {
+                    _Next.HandleRequest(car);
+                }
+            }
+        }
+        
+    }
+
+    class MechanicHandler : Check
+    {
+
+        public override void HandleRequest(Car car)
+        {
+            int testResult = new Random().Next(1, 10);
+            if (testResult <= 5)
+            {
+                car.HasProblam = true;
+                car.Error = "Problam found in Mechanic Check ";
+            }
+            else
+            {
+                car.HasProblam = false;
+                if (_Next != null)
+                {
+                    _Next.HandleRequest(car);
+                }
+            }
+        }
+    }
+
+    class ElectricianHandler : Check
+    {
+
+        public override void HandleRequest(Car car)
+        {
+            int testResult = new Random().Next(1, 10);
+            if (testResult <= 5)
+            {
+                car.HasProblam = true;
+                car.Error = "Problam found in Electrican Check ";
+            }
+            else
+            {
+                car.HasProblam = false;
+                if (_Next != null)
+                {
+                    _Next.HandleRequest(car);
+                }
+            }
+        }
+
+    }
+
+    class ExpertHandler : Check
+    {
+
+        public override void HandleRequest(Car car)
+        {
+            int testResult = new Random().Next(1, 10);
+            if (testResult <= 5)
+            {
+                car.HasProblam = true;
+                car.Error = "Problam found in Expert Check ";
+            }
+            else
+            {
+                car.HasProblam = false;
+                if (_Next != null)
+                {
+                    _Next.HandleRequest(car);
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+}
+
---
+++ ChinOfResponsibilityPractice/ChinOfResponsibilityPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using ChinOfResponsibilityPractice;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Car entering to the Garage");
+        Car car = new Car() { Model = "KIA Niro 2018" };
+        BasicCarHandler basicCarHandler = new BasicCarHandler();
+        MechanicHandler mechanicHandler = new MechanicHandler();
+        ElectricianHandler electricianHandler = new ElectricianHandler();
+        ExpertHandler expertHandler = new ExpertHandler();
+
+        Console.WriteLine("Starting the tests....\n");
+        basicCarHandler.SetNext(mechanicHandler);
+        mechanicHandler.SetNext(electricianHandler);
+        electricianHandler.SetNext(expertHandler);
+
+        Console.WriteLine("Finding where is the problam: \n");
+        basicCarHandler.HandleRequest(car);
+
+        if(car.HasProblam)
+        {
+            Console.WriteLine($"{car.Model} {car.Error}");
+        }
+        else
+        {
+            Console.WriteLine("No problam where found ! ");
+        }
+
+
+    }
+
+}
